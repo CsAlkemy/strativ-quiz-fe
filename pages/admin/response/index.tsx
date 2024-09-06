fcaa0f8942fabdaca1e3f9a6ed5d64b96e3013e3
CsAlkemy <alkemy48@gmail.com>
2024-09-06 12:17:43 +0600

    feat: added all the features so far 2

--- pages/admin/response/index.tsx
+++ pages/admin/response/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import AuthGuard from '@components/shared/custom/user-guard';
 import Head from 'next/head';
-import AdminManageQuiz from '@components/admin/manage-quiz';
 
 export default function AdminPanelPage(): JSX.Element {
     return (
         <>
             <Head>
-                <title>Manage Quiz-StrativQuiz</title>
+                <title>Response-StrativQuiz</title>
             </Head>
             <AuthGuard authRequired={true} title="">
-                <AdminManageQuiz />
+                <div>Response</div>
             </AuthGuard>
         </>
     );
