57526f713c458c0c8c7774695a31a5908fb3df54
CsAlkemy <alkemy48@gmail.com>
2024-09-06 12:23:51 +0600

    fix: padding in mobile

--- components/user/layout/index.tsx
+++ components/user/layout/index.tsx
@@ -7,7 +7,7 @@ interface CommonLayoutProps {
 const UserLayout = ({ children, title }: CommonLayoutProps) => {
     return (
         <>
-            <div className="max-w-screen-md mx-auto">
+            <div className="max-w-screen-md mx-auto px-5 sm:px-0">
                 {title && <h1 className="text-heading-4 font-semibold mb-3">{title}</h1>}
                 {children}
             </div>
